8f45d7a1650cc50807602a3d33f70f0f55556459
Zoreno <metallica317@spray.se>
2021-01-25 15:47:04 +0100

    Formatting of mm_bitmap.c.

--- kernel/src/mm/mm_bitmap.c
+++ kernel/src/mm/mm_bitmap.c
@@ -3,9 +3,9 @@
  * @author Joakim Bertils
  * @version 0.1
  * @date 2019-06-22
- * 
- * @brief 
- * 
+ *
+ * @brief
+ *
  * @copyright Copyright (C) 2019,
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #include <mm/mm_bitmap.h>
@@ -86,7 +86,9 @@ uint64_t mm_bitmap_first_free(mm_bitmap_t bitmap, size_t bitmap_size)
     return -1;
 }
 
-uint64_t mm_bitmap_first_free_s(mm_bitmap_t bitmap, size_t bitmap_size, size_t n)
+uint64_t mm_bitmap_first_free_s(mm_bitmap_t bitmap,
+                                size_t bitmap_size,
+                                size_t n)
 {
     if (!bitmap)
     {
