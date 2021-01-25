ab289fdc662279bc992a0962a990e675cf407e04
Zoreno <metallica317@spray.se>
2021-01-26 00:04:49 +0100

    Formatted IDT and re-enabled backtrace on exception.

--- kernel/src/arch/x86-64/idt.c
+++ kernel/src/arch/x86-64/idt.c
@@ -3,9 +3,9 @@
  * @author Joakim Bertils
  * @version 0.1
  * @date 2019-06-22
- * 
- * @brief 
- * 
+ *
+ * @brief
+ *
  * @copyright Copyright (C) 2019,
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #include <arch/x86-64/idt.h>
@@ -122,9 +122,14 @@ void print_regs(system_stack_t *regs)
         int_name = get_interrupt_name(regs->int_no);
     }
 
-    printf("[IRQ] ==========================================================================\n");
+    printf(
+        "[IRQ] "
+        "======================================================================"
+        "====\n");
     printf("[IRQ] Unhandled Exception %i (Error code: %i) (%s)\n",
-           regs->int_no, regs->err_code, int_name);
+           regs->int_no,
+           regs->err_code,
+           int_name);
 
     // Special printout for page faults
     if (regs->int_no == 14)
@@ -139,32 +144,42 @@ void print_regs(system_stack_t *regs)
 
     printf("[IRQ] Register dump:\n");
     printf("[IRQ] RAX: %#016x RBX: %#016x RCX: %#016x\n",
-           regs->rax, regs->rbx, regs->rcx);
+           regs->rax,
+           regs->rbx,
+           regs->rcx);
     printf("[IRQ] RDX: %#016x RDI: %#016x RSI: %#016x\n",
-           regs->rdx, regs->rdi, regs->rsi);
-    printf("[IRQ] RBP: %#016x RIP: %#016x\n",
-           regs->rbp, regs->rip);
+           regs->rdx,
+           regs->rdi,
+           regs->rsi);
+    printf("[IRQ] RBP: %#016x RIP: %#016x\n", regs->rbp, regs->rip);
 
     printf("[IRQ]  R8: %#016x  R9: %#016x R10: %#016x \n",
-           regs->r8, regs->r9, regs->r10);
+           regs->r8,
+           regs->r9,
+           regs->r10);
     printf("[IRQ] R11: %#016x R12: %#016x R13: %#016x \n",
-           regs->r11, regs->r12, regs->r13);
-    printf("[IRQ] R14: %#016x R15: %#016x\n",
-           regs->r14, regs->r15);
+           regs->r11,
+           regs->r12,
+           regs->r13);
+    printf("[IRQ] R14: %#016x R15: %#016x\n", regs->r14, regs->r15);
 
     printf("[IRQ]  CS: %#016x USERRSP: %#016x SS: %#016x\n",
-           regs->cs, regs->userrsp, regs->ss);
-    printf("[IRQ] RFLAGS: %064b\n",
-           regs->rflags);
+           regs->cs,
+           regs->userrsp,
+           regs->ss);
+    printf("[IRQ] RFLAGS: %064b\n", regs->rflags);
     printf("[IRQ] Backtrace: \n");
 
     kernel_lookup_symbol((void *)regs->rip);
 
     printf("\n");
 
-    //print_backtrace((unsigned long long int *)regs->rbp);
+    print_backtrace((unsigned long long int *)regs->rbp);
 
-    printf("[IRQ] ==========================================================================\n");
+    printf(
+        "[IRQ] "
+        "======================================================================"
+        "====\n");
 }
 
 static IRQ_HANDLER _irq_handlers[256] = {0};
@@ -173,7 +188,6 @@ void arch_x86_64_default_irq_handler(system_stack_t *regs)
 {
     if (regs->int_no >= 32 && regs->int_no < 48)
     {
-
         int irq = regs->int_no - 32;
 
         if (_irq_handlers[irq])
@@ -221,7 +235,10 @@ arch_x86_64_idt_descriptor *arch_x86_64_get_ir(uint32_t i)
     return &_idt[i];
 }
 
-void arch_x86_64_install_ir(uint32_t i, uint16_t flags, uint16_t sel, INT_HANDLER int_handler)
+void arch_x86_64_install_ir(uint32_t i,
+                            uint16_t flags,
+                            uint16_t sel,
+                            INT_HANDLER int_handler)
 {
     if (i > ARCH_X86_64_IDT_MAX_INTERRUPTS)
     {
@@ -305,71 +322,269 @@ void arch_x86_64_initialize_idt(uint16_t code_sel)
 {
     log_info("[ARCH] Initializing IDT...");
 
-    _idtr.limit = (sizeof(arch_x86_64_idt_descriptor)) * ARCH_X86_64_IDT_MAX_INTERRUPTS - 1;
+    _idtr.limit =
+        (sizeof(arch_x86_64_idt_descriptor)) * ARCH_X86_64_IDT_MAX_INTERRUPTS -
+        1;
     _idtr.base = (uint64_t)&_idt[0];
 
     // Install all hardware exception handlers
 
-    arch_x86_64_install_ir(0, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_0);
-    arch_x86_64_install_ir(1, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_1);
-    arch_x86_64_install_ir(2, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_2);
-    arch_x86_64_install_ir(3, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_3);
-    arch_x86_64_install_ir(4, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_4);
-    arch_x86_64_install_ir(5, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_5);
-    arch_x86_64_install_ir(6, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_6);
-    arch_x86_64_install_ir(7, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_7);
-    arch_x86_64_install_ir(8, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_8);
-    arch_x86_64_install_ir(9, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_9);
+    arch_x86_64_install_ir(
+        0,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_0);
+    arch_x86_64_install_ir(
+        1,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_1);
+    arch_x86_64_install_ir(
+        2,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_2);
+    arch_x86_64_install_ir(
+        3,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_3);
+    arch_x86_64_install_ir(
+        4,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_4);
+    arch_x86_64_install_ir(
+        5,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_5);
+    arch_x86_64_install_ir(
+        6,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_6);
+    arch_x86_64_install_ir(
+        7,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_7);
+    arch_x86_64_install_ir(
+        8,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_8);
+    arch_x86_64_install_ir(
+        9,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_9);
 
-    arch_x86_64_install_ir(10, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_10);
-    arch_x86_64_install_ir(11, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_11);
-    arch_x86_64_install_ir(12, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_12);
-    arch_x86_64_install_ir(13, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_13);
-    arch_x86_64_install_ir(14, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_14);
-    arch_x86_64_install_ir(15, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_15);
-    arch_x86_64_install_ir(16, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_16);
-    arch_x86_64_install_ir(17, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_17);
-    arch_x86_64_install_ir(18, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_18);
-    arch_x86_64_install_ir(19, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_19);
+    arch_x86_64_install_ir(
+        10,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_10);
+    arch_x86_64_install_ir(
+        11,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_11);
+    arch_x86_64_install_ir(
+        12,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_12);
+    arch_x86_64_install_ir(
+        13,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_13);
+    arch_x86_64_install_ir(
+        14,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_14);
+    arch_x86_64_install_ir(
+        15,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_15);
+    arch_x86_64_install_ir(
+        16,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_16);
+    arch_x86_64_install_ir(
+        17,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_17);
+    arch_x86_64_install_ir(
+        18,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_18);
+    arch_x86_64_install_ir(
+        19,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_19);
 
-    arch_x86_64_install_ir(20, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_20);
-    arch_x86_64_install_ir(21, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_21);
-    arch_x86_64_install_ir(22, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_22);
-    arch_x86_64_install_ir(23, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_23);
-    arch_x86_64_install_ir(24, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_24);
-    arch_x86_64_install_ir(25, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_25);
-    arch_x86_64_install_ir(26, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_26);
-    arch_x86_64_install_ir(27, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_27);
-    arch_x86_64_install_ir(28, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_28);
-    arch_x86_64_install_ir(29, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_29);
+    arch_x86_64_install_ir(
+        20,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_20);
+    arch_x86_64_install_ir(
+        21,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_21);
+    arch_x86_64_install_ir(
+        22,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_22);
+    arch_x86_64_install_ir(
+        23,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_23);
+    arch_x86_64_install_ir(
+        24,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_24);
+    arch_x86_64_install_ir(
+        25,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_25);
+    arch_x86_64_install_ir(
+        26,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_26);
+    arch_x86_64_install_ir(
+        27,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_27);
+    arch_x86_64_install_ir(
+        28,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_28);
+    arch_x86_64_install_ir(
+        29,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_29);
 
-    arch_x86_64_install_ir(30, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_30);
-    arch_x86_64_install_ir(31, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_31);
+    arch_x86_64_install_ir(
+        30,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_30);
+    arch_x86_64_install_ir(
+        31,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_31);
 
     // Install the System call handler
 
-    arch_x86_64_install_ir(128, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_isr_128);
+    arch_x86_64_install_ir(
+        128,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_isr_128);
 
     // TInstall IRQ Handlers
 
 #define IRQ_BASE 32
 
-    arch_x86_64_install_ir(IRQ_BASE + 0, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_0);
-    arch_x86_64_install_ir(IRQ_BASE + 1, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_1);
-    arch_x86_64_install_ir(IRQ_BASE + 2, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_2);
-    arch_x86_64_install_ir(IRQ_BASE + 3, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_3);
-    arch_x86_64_install_ir(IRQ_BASE + 4, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_4);
-    arch_x86_64_install_ir(IRQ_BASE + 5, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_5);
-    arch_x86_64_install_ir(IRQ_BASE + 6, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_6);
-    arch_x86_64_install_ir(IRQ_BASE + 7, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_7);
-    arch_x86_64_install_ir(IRQ_BASE + 8, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_8);
-    arch_x86_64_install_ir(IRQ_BASE + 9, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_9);
-    arch_x86_64_install_ir(IRQ_BASE + 10, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_10);
-    arch_x86_64_install_ir(IRQ_BASE + 11, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_11);
-    arch_x86_64_install_ir(IRQ_BASE + 12, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_12);
-    arch_x86_64_install_ir(IRQ_BASE + 13, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_13);
-    arch_x86_64_install_ir(IRQ_BASE + 14, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_14);
-    arch_x86_64_install_ir(IRQ_BASE + 15, ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT, code_sel, arch_x86_64_irq_15);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 0,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_0);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 1,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_1);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 2,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_2);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 3,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_3);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 4,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_4);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 5,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_5);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 6,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_6);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 7,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_7);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 8,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_8);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 9,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_9);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 10,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_10);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 11,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_11);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 12,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_12);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 13,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_13);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 14,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_14);
+    arch_x86_64_install_ir(
+        IRQ_BASE + 15,
+        ARCH_X86_64_IDT_DESC_BIT32 | ARCH_X86_64_IDT_DESC_PRESENT,
+        code_sel,
+        arch_x86_64_irq_15);
 
     arch_x86_64_idt_flush((uint64_t)&_idtr);
 
