81e04cf39d124c9f586cf9493726de507b6f2606
Joakim Bertils <metallica317@spray.se>
2021-02-04 09:47:55 +0100

    Updated external linkage of some functions in the ABI.
    There were a mismatch between the header file and the source file
    regarding the linkage of the functions __cxa_pure_virtual and
    __cxa_deleted_virtual.

--- libsupc++/include/cxxabi.h
+++ libsupc++/include/cxxabi.h
@@ -35,8 +35,11 @@ class type_info;
 
 namespace __cxxabiv1
 {
-void __cxa_pure_virtual(void) __attribute__((__noreturn__));
-void __cxa_deleted_virtual(void) __attribute__((__noreturn__));
+extern "C"
+{
+    void __cxa_pure_virtual(void) __attribute__((__noreturn__));
+    void __cxa_deleted_virtual(void) __attribute__((__noreturn__));
+}
 } // namespace __cxxabiv1
 
 extern "C"
