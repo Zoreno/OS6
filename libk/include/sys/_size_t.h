e4912878bfe0cc66873b746e3b53928a31e52048
Joakim Bertils <metallica317@spray.se>
2021-05-08 11:26:19 +0200

    Add ssize_t as signed variant of size_t.

--- libk/include/sys/_size_t.h
+++ libk/include/sys/_size_t.h
@@ -3,9 +3,9 @@
  * @author Joakim Bertils
  * @version 0.1
  * @date 2019-04-25
- * 
+ *
  * @brief Defines a type that can reperent an array size
- * 
+ *
  * @copyright Copyright (C) 2019,
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
- * 
+ *
  */
 
 #ifndef _LIBK__SIZE_T_H
@@ -29,10 +29,17 @@
 
 /**
  * @brief The array size type
- * 
- * 
+ *
+ *
  */
 typedef unsigned long long size_t;
+
+/**
+ * @brief The array size signed type
+ *
+ *
+ */
+typedef long long ssize_t;
 
 #endif
 
