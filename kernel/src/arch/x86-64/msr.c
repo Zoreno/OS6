17de8be7452bd1419ef1522889d90fafcb939359
Joakim Bertils <metallica317@spray.se>
2021-07-09 20:39:19 +0200

    Add file header and end of file marker to msr.c

--- kernel/src/arch/x86-64/msr.c
+++ kernel/src/arch/x86-64/msr.c
@@ -1,3 +1,25 @@
+/**
+ * @file msr.c
+ * @author Joakim Bertils
+ * @version 0.1
+ * @date 2021-07-09
+ *
+ * @brief
+ *
+ * @copyright Copyright (C) 2021,
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+ *
+ */
+
 #include <arch/x86-64/msr.h>
 
 uint64_t rdmsr(msr_registers_t msr)
@@ -17,3 +39,7 @@ void wrmsr(msr_registers_t msr, uint64_t value)
 
     __asm__ volatile("wrmsr" ::"c"((uint64_t)msr), "a"(low), "d"(high));
 }
+
+//=============================================================================
+// End of file
+//=============================================================================
